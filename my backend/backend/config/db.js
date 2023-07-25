16ca274bdcb5f6743f75210f10e6556acf39045e
Trương Hà Trâm Anh <94051542+nanaliuchuang@users.noreply.github.com>
2023-07-25 18:56:16 +0700

    Update db.js

--- my backend/backend/config/db.js
+++ my backend/backend/config/db.js
@@ -4,7 +4,8 @@ import colors from "colors";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://nanaliuchuang:k66zsv94lvGIHsbQ@mystore.imdgcej.mongodb.net/myproject", {
+    const conn = await mongoose.connect("mongodb+srv://nanaliuchuang:k66zsv94lvGIHsbQ@mystore.imdgcej.mongodb.net/myproject?retryWrites=true&w=majority
+", {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
